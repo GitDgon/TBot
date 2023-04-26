5bbcdbfbf167ec53c5c3eec6a7d2ca1060696abd
GitDgon <dgon_mail@mail.ru>
2023-04-26 21:06:16 +0500

    Add token.txt

--- tbot.py
+++ tbot.py
@@ -1,17 +1,17 @@
 import telebot
 
-bot = telebot.TeleBot('token.txt')
 TOKEN = None
 
-with open("token.txt") as f:
+
+
+with open('token.txt') as f:
     TOKEN = f.read().strip()
+
+bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(messege):
     bot.send_message(messege.chat.id, '<b>Привет!</b>', parse_mode='html')
-
-
-
 
 
 
