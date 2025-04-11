e3edba52e3e5f8de12bdcb4ee616f3286e3e7f7b
GitDgon <dgon_mail@mail.ru>
2025-04-11 20:17:05 +0500

    Add pingtest

--- tbot.py
+++ tbot.py
@@ -1,27 +1,73 @@
 import telebot
 import webbrowser
+import datetime
+import threading
 
 TOKEN = None
 with open('token.txt') as f:
     TOKEN = f.read().strip()
 
 bot = telebot.TeleBot(TOKEN)
+users = {}
+print(type(users))
+print(users)
+
+
+@bot.message_handler(commands=['start'])
+# декоратор message_handler, он позволяет сообщить телеграму какую команду мы отслеживаем (сама команда передаётся в списке commands)
+def welcom(message):
+    chat_id = message.chat.id
+    bot.send_message(chat_id, 'Добрый день! Введи имя!')
+    users[chat_id] = {}
+    print(users)
+    # register_next_step_handler - функция позволяющая задать следующий шаг бота
+    # В этой функции мы указываем два аргумента:
+    # message - основной объект в телеграм ботах,
+    # который содержит всю необходимую информацию (id чата, текст сообщения и тд), а также ссылка на функцию, которая будет далее вызываться.
+    bot.register_next_step_handler(message, save_username)
+
+def save_username(message):
+    chat_id = message.chat.id
+    name = message.text
+
+
+    pass
+
+
+
+
+
+
+
 
 @bot.message_handler(commands=['site', 'web'])
 def site(massage):
     webbrowser.open('https://www.mail.ru')
 
 
+@bot.message_handler(commands=['test'])
+def start(message):
+    print(message.chat.id)
+
+
+@bot.message_handler(commands=['ping'])
+def command(message):
+    baner = str('pingtest')
+    chat_id = -1002654832876
+    print(type(chat_id))
+    print(chat_id)
+    bot.send_message(chat_id, f'Проверка: {baner}')
 
 
 @bot.message_handler(commands=['date'])
 def start(message):
     print(message.from_user.username)
+    print(message.chat.id)
     if message.from_user.username is None:
-        bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name} {message.from_user.language_code}')
+        bot.send_message(message.chat.id, f'Привет, {message.from_user}')
+        # bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name} {message.from_user.language_code}')
     else:
         bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name} {message.from_user.username}')
-
 
 
 @bot.message_handler(commands=['help'])
@@ -36,5 +82,7 @@ def info(message):
     elif message.text.lower() == '/id':
         bot.reply_to(message, f'ID: {message.from_user.id}')
 
-
+# if __name__ == '__main__':
+#     print('Бот запущен!')
+    # bot.infinity_polling(none_stop=True)
 bot.polling(none_stop=True)
